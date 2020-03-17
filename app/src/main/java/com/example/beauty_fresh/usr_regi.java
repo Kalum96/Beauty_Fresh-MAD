4a0ef2f0da478803d40f060b6e9cb137348d2294
Rajitha_PC <jayasekararajitha@gmail.com>
2020-03-18 03:08:12 +0530

    ..

---
+++ app/src/main/java/com/example/beauty_fresh/usr_regi.java
@@ -0,0 +1,14 @@
+package com.example.beauty_fresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class usr_regi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usr_regi);
+    }
+}
